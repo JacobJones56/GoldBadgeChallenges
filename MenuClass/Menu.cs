8cfd67bfe5885ac19aa4628cccb9b4d8a356a488
Jacob <jacobeljones56@gmail.com>
2021-09-30 18:53:05 -0400

    Add project files.

---
+++ MenuClass/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuClass
+{
+    public class Menu
+    {
+        public string FoodName { get; set; }
+        public int FoodNumber { get; set; }
+        public int FoodId { get; set; }
+        public string FoodInfo { get; set; }
+        public string FoodPrice { get; set; }
+
+        public Menu() { }
+
+        public Menu(string foodName, int foodNumber, int foodId, string foodInfo, string foodPrice)
+        {
+            FoodName = foodName;
+            FoodNumber = foodNumber;
+            FoodId = foodId;
+            FoodInfo = foodInfo;
+            FoodPrice = foodPrice;
+        }
+    }
+}
---
+++ MenuRepo/MenuRepoMethods.cs
@@ -0,0 +1,62 @@
+﻿using MenuClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuRepo
+{
+    public class MenuRepoMethods
+    {
+        private List<Menu> _listOfMenuOptions = new List<Menu>();
+
+        // Create
+        public void CreateNewMenuOption(Menu option)
+        {
+            _listOfMenuOptions.Add(option);
+        }
+        // Read
+        public List<Menu> MenuList()
+        {
+            return _listOfMenuOptions;
+        }
+        // Delete
+        public bool RemoveMenuOption(int id)
+        {
+            Menu menuOption = GetIdOfMenuOption(id);
+
+            if (menuOption == null)
+            {
+                return false;
+            }
+
+            int count = _listOfMenuOptions.Count;
+            _listOfMenuOptions.Remove(menuOption);
+
+            if (count > _listOfMenuOptions.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        // Helper method
+        public Menu GetIdOfMenuOption(int id)
+        {
+            foreach (Menu menuOption in _listOfMenuOptions)
+            {
+                if (menuOption.FoodId == id)
+                {
+                    return menuOption;
+                }
+            }
+            return null;
+        }
+    }
+}
